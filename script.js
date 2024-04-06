4b7a3d098d47b86e9f2f1d2c153b521e6cf18d21
HunterTigerX <HunterTigerX@gmail.com>
2024-04-06 19:55:45 +0300

    feat: update site

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const linkToMain = document.querySelector(".linkToMain");
 const linkToPve = document.querySelector(".linkToPve");
-const linkToPve2 = document.querySelector(".linkToPve2");
+// const linkToPve2 = document.querySelector(".linkToPve2");
 const linkToFAQ = document.querySelector(".linkToFAQ");
 const linkToMaterials = document.querySelector(".linkToMaterials");
 const linkToCourrier = document.querySelector(".linkToCourrier");
 const main = document.querySelector(".blocks-wrapper");
 const pve = document.querySelector(".pve-wrapper");
-const pve2 = document.querySelector(".pve-wrapper2");
+// const pve2 = document.querySelector(".pve-wrapper2");
 const faq = document.querySelector(".faq-wrapper");
 const materials = document.querySelector(".materials-wrapper");
 // const alliance = document.querySelector('.alliance-wrapper');
@@ -19,7 +19,7 @@ document.cookie =
 function saveDislays() {
   localStorage.setItem("main", main.style.display);
   localStorage.setItem("pve", pve.style.display);
-  localStorage.setItem("pve2", pve2.style.display);
+  // // localStorage.setItem("pve2", pve2.style.display);
   localStorage.setItem("faq", faq.style.display);
   localStorage.setItem("materials", materials.style.display);
 }
@@ -27,7 +27,7 @@ function saveDislays() {
 function restoreDisplays() {
   main.style.display = localStorage.getItem("main");
   pve.style.display = localStorage.getItem("pve");
-  pve2.style.display = localStorage.getItem("pve2");
+  // // pve2.style.display = localStorage.getItem("pve2");
   faq.style.display = localStorage.getItem("faq");
   materials.style.display = localStorage.getItem("materials");
 }
@@ -42,7 +42,7 @@ restoreDisplays();
 linkToMain.addEventListener("click", (e) => {
   main.style.display = "flex";
   pve.style.display = "none";
-  pve2.style.display = "none";
+  // pve2.style.display = "none";
   faq.style.display = "none";
   materials.style.display = "none";
   saveDislays();
@@ -51,25 +51,25 @@ linkToMain.addEventListener("click", (e) => {
 linkToPve.addEventListener("click", (e) => {
   main.style.display = "none";
   pve.style.display = "flex";
-  pve2.style.display = "none";
+  // pve2.style.display = "none";
   faq.style.display = "none";
   materials.style.display = "none";
   saveDislays();
 });
 
-linkToPve2.addEventListener("click", (e) => {
-  main.style.display = "none";
-  pve.style.display = "none";
-  pve2.style.display = "flex";
-  faq.style.display = "none";
-  materials.style.display = "none";
-  saveDislays();
-});
+// linkToPve2.addEventListener("click", (e) => {
+  // main.style.display = "none";
+  // pve.style.display = "none";
+  // pve2.style.display = "flex";
+  // faq.style.display = "none";
+  // materials.style.display = "none";
+  // saveDislays();
+// });
 
 linkToFAQ.addEventListener("click", (e) => {
   main.style.display = "none";
   pve.style.display = "none";
-  pve2.style.display = "none";
+  // pve2.style.display = "none";
   faq.style.display = "flex";
   materials.style.display = "none";
   saveDislays();
@@ -78,7 +78,7 @@ linkToFAQ.addEventListener("click", (e) => {
 linkToMaterials.addEventListener("click", (e) => {
   main.style.display = "none";
   pve.style.display = "none";
-  pve2.style.display = "none";
+  // pve2.style.display = "none";
   faq.style.display = "none";
   materials.style.display = "flex";
   saveDislays();
